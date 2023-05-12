5f302827f85089ecc68793b8e4fb238bad03beac
Denis Moreira <silvad281@gmail.com>
2023-05-12 13:33:29 -0300

    atualização do app

--- app.js
+++ app.js
@@ -56,11 +56,71 @@ class Bd {
          if (despesa === null) {
             continue;
          }
-
+         despesa.id = i;
          despesas.push(despesa);
       }
 
       return despesas;
+   }
+
+   pesquisar(despesa) {
+      let despesasFiltradas = Array();
+      despesasFiltradas = this.recuperarTodosRegistros();
+      console.log(despesasFiltradas);
+      console.log(despesa);
+
+      //ano
+      if (despesa.ano != "") {
+         console.log("filtro de ano");
+         despesasFiltradas = despesasFiltradas.filter(
+            (d) => d.ano == despesa.ano
+         );
+      }
+
+      //mes
+      if (despesa.mes != "") {
+         console.log("filtro de mes");
+         despesasFiltradas = despesasFiltradas.filter(
+            (d) => d.mes == despesa.mes
+         );
+      }
+
+      //dia
+      if (despesa.dia != "") {
+         console.log("filtro de dia");
+         despesasFiltradas = despesasFiltradas.filter(
+            (d) => d.dia == despesa.dia
+         );
+      }
+
+      //tipo
+      if (despesa.tipo != "") {
+         console.log("filtro de tipo");
+         despesasFiltradas = despesasFiltradas.filter(
+            (d) => d.tipo == despesa.tipo
+         );
+      }
+
+      //descricao
+      if (despesa.descricao != "") {
+         console.log("filtro de descricao");
+         despesasFiltradas = despesasFiltradas.filter(
+            (d) => d.descricao == despesa.descricao
+         );
+      }
+
+      //valor
+      if (despesa.valor != "") {
+         console.log("filtro de valor");
+         despesasFiltradas = despesasFiltradas.filter(
+            (d) => d.valor == despesa.valor
+         );
+      }
+
+      return despesasFiltradas;
+   }
+   remover(id) {
+      localStorage.removeItem(id);
    }
 }
 
@@ -97,6 +157,13 @@ function cadastrarDespesa() {
 
       //dialog de sucesso
       $("#modalRegistraDespesa").modal("show");
+
+      ano.value = "";
+      mes.value = "";
+      dia.value = "";
+      tipo.value = "";
+      descricao.value = "";
+      valor.value = "";
    } else {
       document.getElementById("modal_titulo").innerHTML =
          "Erro na inclusão do registro";
@@ -112,10 +179,10 @@ function cadastrarDespesa() {
    }
 }
 
-function carregaListaDespesas() {
-   let despesas = Array();
-
-   despesas = bd.recuperarTodosRegistros();
+function carregaListaDespesas(despesas = Array(), filtro = false) {
+   if (despesas.length == 0 && filtro == false) {
+      despesas = bd.recuperarTodosRegistros();
+   }
 
    /*
 
@@ -129,7 +196,7 @@ function carregaListaDespesas() {
 	*/
 
    let listaDespesas = document.getElementById("listaDespesas");
-
+   listaDespesas.innerHTML = "";
    despesas.forEach(function (d) {
       //Criando a linha (tr)
       var linha = listaDespesas.insertRow();
@@ -158,6 +225,34 @@ function carregaListaDespesas() {
       linha.insertCell(1).innerHTML = d.tipo;
       linha.insertCell(2).innerHTML = d.descricao;
       linha.insertCell(3).innerHTML = d.valor;
+
+      //criar o botão de exclusão
+      let btn = document.createElement("button");
+      btn.className = "btn btn-danger";
+      btn.innerHTML = '<i class="fas fa-times"></i>';
+      btn.id = `id_despesa_${d.id}`;
+      btn.onclick = function () {
+         let id = this.id.replace("id_despesa_", "");
+
+         bd.remover(id);
+         window.location.reload();
+      };
+      linha.insertCell(4).append(btn);
       console.log(d);
    });
+}
+
+function pesquisarDespesa() {
+   let ano = document.getElementById("ano").value;
+   let mes = document.getElementById("mes").value;
+   let dia = document.getElementById("dia").value;
+   let tipo = document.getElementById("tipo").value;
+   let descricao = document.getElementById("descricao").value;
+   let valor = document.getElementById("valor").value;
+
+   let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor);
+
+   let despesas = bd.pesquisar(despesa);
+
+   this.carregaListaDespesas(despesas, true);
 }
